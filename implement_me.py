cb16c9789bb7ebad231bfa17c46cb4d664eb191f
amin-oueslati <amin.m.oueslati@gmail.com>
2023-09-11 09:30:53 +0200

    wip

--- implement_me.py
+++ implement_me.py
@@ -1,5 +1,19 @@
-
-def lcm(a, b):
+class LCM_calculator:
   """compute Least Common Multiple between two integers."""
 
-  pass
+  def calculate_lcm(self, a, b):
+    # Calculate the LCM using the formula LCM(a, b) = (a * b) / GCD(a, b)
+    gcd = self.calculate_gcd(a, b)
+    return (a * b) // gcd
+
+  def calculate_gcd(self, a, b):
+    # Calculate the GCD using the Euclidean algorithm
+    while b:
+        a, b = b, a % b
+    return a
+
+lcm_calculator = LCM_calculator()
+a = 2
+b = 3
+lcm = lcm_calculator(a, b)
+print(f"The LCM of {a} and {b} is {lcm}")
